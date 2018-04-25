e6e36c431db8997f59851e774f0542f8ae9ea4f8
siyer13 <sreedharriyer@gmail.com>
2018-04-24 22:08:28 -0700

    calculate commute

---
+++ commute.R
@@ -0,0 +1,14 @@
+commute = c(17, 16, 20, 24, 22, 15, 21, 15, 17, 22)
+
+max(commute)
+mean(commute)
+min(commute)
+
+ which(commute == 24)
+
+sum(commute > 20)
+
+
+max(commute)
+mean(commute)
+min(commute)
