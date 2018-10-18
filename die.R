5e3d22fcb173a2793d01b6410a9fa089caeec501
Sridhar <sreedharriyer@gmail.com>
2018-10-18 22:46:35 +0530

    Updated die

--- die.R
+++ die.R
@@ -1,3 +1,7 @@
 die <- 1:6
-sample(x=die, size=1)
+sample(x=die, size=2) 
+# the above is not accurate as the sample is without replacement
+# die should be independent of the previous one
+sample(x=die, size=2, replace=TRUE)
+
 
