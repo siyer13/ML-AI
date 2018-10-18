7163efb07f150e44574a8632da358b1885557696
Sridhar <sreedharriyer@gmail.com>
2018-10-18 22:55:09 +0530

    dice roll

---
+++ dice_roll.R
@@ -0,0 +1,5 @@
+roll <- function() {
+  die <- 1:6
+  dice <- sample(die,size=2,replace=TRUE)
+  dice
+}
--- die.R
+++ die.R
@@ -3,5 +3,3 @@ sample(x=die, size=2)
 # the above is not accurate as the sample is without replacement
 # die should be independent of the previous one
 sample(x=die, size=2, replace=TRUE)
-
-
