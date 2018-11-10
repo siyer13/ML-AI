a6a80f85565893884b83a8332a37a35aa7f21d79
Sridhar <sreedharriyer@gmail.com>
2018-11-10 23:22:46 +0530

    Basic dataypes - Vector

---
+++ basic_datatypes.R
@@ -0,0 +1,7 @@
+# In R programming, the very basic data types are the R-objects called vectors
+# they hold elements of different classes
+
+# create a vector
+
+apple <- c('green', 'red', 'yellow')
+print(class(apple))
