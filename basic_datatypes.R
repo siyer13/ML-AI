2706bd96f57bacaaadd67300a4b2848434fb4779
Sridhar <sreedharriyer@gmail.com>
2018-11-10 23:25:46 +0530

    Basic dataypes - List

--- basic_datatypes.R
+++ basic_datatypes.R
@@ -4,4 +4,15 @@
 # create a vector
 
 apple <- c('green', 'red', 'yellow')
+print(apple)
 print(class(apple))
+
+#List
+# A list is an R-object which can contain many different types of elements 
+# inside it like vectors, functions and even another list inside it.
+
+#create a list
+
+mylist <- list(c(1,2,3),2.3,"hello")
+print(mylist)
+print(class(mylist))
