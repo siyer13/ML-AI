8697507188d2874795ad78dc80b4b2a89c70f786
Sridhar <sreedharriyer@gmail.com>
2019-01-04 21:16:10 +0530

    Build dataset - Read data - validate data - summarize data

--- iris/R/read_dataset.R
+++ iris/R/read_dataset.R
@@ -4,10 +4,7 @@ filename <- "data/iris.csv"
 # load the csv file from local directory
 dataset <- read.csv(filename, header=FALSE)
 
-# test if the file loaded properly
-head(dataset)
-
-
 colnames(dataset) <- c("Sepal.Length","Sepal.Width","Petal.Length","Petal.Width","Species")
 
+# test if the file loaded properly
 head(dataset)
